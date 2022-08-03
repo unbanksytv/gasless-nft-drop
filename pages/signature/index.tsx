823f8702206c85eba096356ad2834d73870967e4
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-08-03 21:09:00 +0200

    marketplace update

---
+++ pages/signature/index.tsx
@@ -0,0 +1,299 @@
+import styles from "./styles/Home.module.css";
+import {
+  ThirdwebNftMedia,
+  useAddress,
+  useDisconnect,
+  useMetamask,
+  useNetwork,
+  useNetworkMismatch,
+  useNFTCollection,
+  useNFTs,
+  useSigner,
+} from "@thirdweb-dev/react";
+import { ChainId, ThirdwebSDK } from "@thirdweb-dev/sdk";
+import type { NextPage } from "next";
+import { useRef, useState } from "react";
+
+const Home: NextPage = () => {
+  // Helpful thirdweb hooks to connect and manage the wallet from metamask.
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  const disconnectWallet = useDisconnect();
+  const signer = useSigner();
+  const isOnWrongNetwork = useNetworkMismatch();
+  const [, switchNetwork] = useNetwork();
+
+  // Fetch the NFT collection from thirdweb via it's contract address.
+  const nftCollection = useNFTCollection(
+    // Replace this with your NFT Collection contract address
+    process.env.NEXT_PUBLIC_NFT_COLLECTION_ADDRESS
+  );
+
+  // Here we store the user inputs for their NFT.
+  const [nftName, setNftName] = useState<string>("");
+  const [file, setFile] = useState<File>();
+
+  const { data: nfts, isLoading: loadingNfts } = useNFTs(nftCollection);
+
+  // Magic to get the file upload even though its hidden
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  // Function to store file in state when user uploads it
+  const uploadFile = () => {
+    if (fileInputRef?.current) {
+      fileInputRef.current.click();
+
+      fileInputRef.current.onchange = () => {
+        if (fileInputRef?.current?.files?.length) {
+          const file = fileInputRef.current.files[0];
+          setFile(file);
+        }
+      };
+    }
+  };
+
+  // This function calls a Next JS API route that mints an NFT with signature-based minting.
+  // We send in the address of the current user, and the text they entered as part of the request.
+  const mintWithSignature = async () => {
+    if (!address) {
+      connectWithMetamask();
+      return;
+    }
+
+    if (isOnWrongNetwork) {
+      switchNetwork && switchNetwork(ChainId.Mumbai);
+      return;
+    }
+
+    try {
+      if (!file || !nftName) {
+        alert("Please enter a name and upload a file.");
+        return;
+      }
+
+      if (!address || !signer) {
+        alert("Please connect to your wallet.");
+        return;
+      }
+
+      // Upload image to IPFS using the sdk.storage
+      const tw = new ThirdwebSDK(signer);
+      const upload = await tw.storage.upload(file);
+      const url = `${upload.uris[0]}`;
+
+      // Make a request to /api/server
+      const signedPayloadReq = await fetch(`/api/server`, {
+        method: "POST",
+        body: JSON.stringify({
+          authorAddress: address, // Address of the current user
+          nftName: nftName,
+          imagePath: url,
+        }),
+      });
+
+      console.log("Received Signed payload", signedPayloadReq);
+
+      // Grab the JSON from the response
+      const json = await signedPayloadReq.json();
+
+      console.log("Json:", json);
+
+      // If the request failed, we'll show an error.
+      if (!signedPayloadReq.ok) {
+        alert(json.error);
+        return;
+      }
+
+      // If the request succeeded, we'll get the signed payload from the response.
+      // The API should come back with a JSON object containing a field called signedPayload.
+      // This line of code will parse the response and store it in a variable called signedPayload.
+      const signedPayload = json.signedPayload;
+
+      // Now we can call signature.mint and pass in the signed payload that we received from the server.
+      // This means we provided a signature for the user to mint an NFT with.
+      const nft = await nftCollection?.signature.mint(signedPayload);
+
+      console.log("Successfully minted NFT with signature", nft);
+
+      alert("Successfully minted NFT with signature");
+
+      return nft;
+    } catch (e) {
+      console.error("An error occurred trying to mint the NFT:", e);
+    }
+  };
+
+  return (
+    <>
+      {/* Header */}
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <div>
+            <a
+              href="https://thirdweb.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={`/logo.png`} alt="Thirdweb Logo" width={135} />
+            </a>
+          </div>
+        </div>
+        <div className={styles.right}>
+          {address ? (
+            <>
+              <a
+                className={styles.secondaryButton}
+                onClick={() => disconnectWallet()}
+              >
+                Disconnect Wallet
+              </a>
+              <p style={{ marginLeft: 8, marginRight: 8, color: "grey" }}>|</p>
+              <p>
+                {address.slice(0, 6).concat("...").concat(address.slice(-4))}
+              </p>
+            </>
+          ) : (
+            <a
+              className={styles.mainButton}
+              onClick={() => connectWithMetamask()}
+            >
+              Connect Wallet
+            </a>
+          )}
+        </div>
+      </div>
+
+      {/* Content */}
+      <div className={styles.container}>
+        {/* Top Section */}
+        <h1 className={styles.h1}>Signature-Based Minting</h1>
+        <p className={styles.explain}>
+          Signature-based minting with{" "}
+          <b>
+            {" "}
+            <a
+              href="https://thirdweb.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.purple}
+            >
+              thirdweb
+            </a>
+          </b>{" "}
+          + Next.JS to create a community-made NFT collection with restrictions.
+        </p>
+
+        <p>
+          Hint: We only give out signatures if your NFT name is a cool{" "}
+          <b>animal name</b>! 😉
+        </p>
+
+        <hr className={styles.divider} />
+
+        <div className={styles.collectionContainer}>
+          <h2 className={styles.ourCollection}>
+            Mint your own NFT into the collection:
+          </h2>
+
+          <input
+            type="text"
+            placeholder="Name of your NFT"
+            className={styles.textInput}
+            maxLength={26}
+            onChange={(e) => setNftName(e.target.value)}
+          />
+
+          {file ? (
+            <img
+              src={URL.createObjectURL(file)}
+              style={{ cursor: "pointer", maxHeight: 250, borderRadius: 8 }}
+              onClick={() => setFile(undefined)}
+            />
+          ) : (
+            <div
+              className={styles.imageInput}
+              onClick={uploadFile}
+              onDragOver={(e) => e.preventDefault()}
+              onDrop={(e) => {
+                e.preventDefault();
+                setFile(e.dataTransfer.files[0]);
+              }}
+            >
+              Drag and drop an image here to upload it!
+            </div>
+          )}
+        </div>
+        <input
+          type="file"
+          accept="image/png, image/gif, image/jpeg"
+          id="profile-picture-input"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+        />
+
+        <div style={{ marginTop: 24 }}>
+          {address ? (
+            <a className={styles.mainButton} onClick={mintWithSignature}>
+              Mint NFT
+            </a>
+          ) : (
+            <a className={styles.mainButton} onClick={connectWithMetamask}>
+              Connect Wallet
+            </a>
+          )}
+        </div>
+
+        <hr className={styles.smallDivider} />
+
+        <div className={styles.collectionContainer}>
+          <h2 className={styles.ourCollection}>
+            Other NFTs in this collection:
+          </h2>
+
+          {loadingNfts ? (
+            <p>Loading...</p>
+          ) : (
+            <div className={styles.nftGrid}>
+              {nfts?.map((nft) => (
+                <div
+                  className={styles.nftItem}
+                  key={nft.metadata.id.toString()}
+                >
+                  <div>
+                    <ThirdwebNftMedia
+                      metadata={nft.metadata}
+                      style={{
+                        height: 90,
+                        borderRadius: 16,
+                      }}
+                    />
+                  </div>
+                  <div style={{ textAlign: "center" }}>
+                    <p>Named</p>
+                    <p>
+                      <b>{nft.metadata.name}</b>
+                    </p>
+                  </div>
+
+                  <div style={{ textAlign: "center" }}>
+                    <p>Owned by</p>
+                    <p>
+                      <b>
+                        {nft.owner
+                          .slice(0, 6)
+                          .concat("...")
+                          .concat(nft.owner.slice(-4))}
+                      </b>
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
