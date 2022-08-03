84a784cb8185afcb6ce68810617515c5831e5230
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-08-03 09:35:45 +0200

    chain ID

---
+++ constants/chainIdToNameMapping.js
@@ -0,0 +1,15 @@
+const chainIdToName = {
+    1: "mainnet",
+    5: "goerli",
+    137: "polygon",
+    80001: "mumbai",
+    250: "fantom",
+    43113: "avalanche-testnet",
+    43114: "avalanche",
+    10: "optimism",
+    69: "optimism-testnet",
+    42161: "arbitrum",
+    421611: "arbitrum-testnet",
+  };
+  
+  export default chainIdToName;
